7a572ba07d2aadce617f6e895c58fc7962078eb9
jiwoo311 <jw.lee@stk-eng.com>
2025-12-10 11:51:00 +0900

    feat :: header - 헤더 생성

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { View, Image, Text } from "react-native";
+import Logo from "../../assets/img/logo.png";
+
+export default function Header() {
+  return (
+    <View
+      style={{
+        height: 70,
+        backgroundColor: "#305685",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        paddingHorizontal: 60,
+        borderBottomWidth: 1,
+        borderColor: "#ddd",
+      }}
+    >
+        <Image
+          source={Logo}
+          style={{ height: 50, width: 200, resizeMode: "contain" }}
+        />
+        <Text style={{ fontSize: 18, fontWeight: "400", color: "white" }}>로그인</Text>
+    </View>
+  );
+}

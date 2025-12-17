1110974a8786018e742ad0846f9a25a561d7eabd
jiwoo311 <jw.lee@stk-eng.com>
2025-12-17 15:54:22 +0900

    feat :: login text on header - 헤더에 로그인 텍스트 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,31 @@
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, TouchableOpacity, Platform } from "react-native";
 import Logo from "../../assets/logo/logo.png";
 
 export default function Header() {
   return (
     <View
       style={{
-        height: 70,
+        paddingTop: Platform.OS === "ios" ? 50 : 20,
+        height: Platform.OS === "ios" ? 110 : 90,
         backgroundColor: "#305685",
-        justifyContent: "space-between",
-        alignItems: "center",
         flexDirection: "row",
-        paddingHorizontal: 60,
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
         borderBottomWidth: 1,
         borderColor: "#ddd",
       }}
     >
-        <Image
-          source={Logo}
-          style={{ height: 50, width: 200, resizeMode: "contain" }}
-        />
-        <Text style={{ fontSize: 18, fontWeight: "400", color: "white" }}>로그인</Text>
+      <Image
+        source={Logo}
+        style={{ height: 40, width: 160, resizeMode: "contain" }}
+      />
+
+      <TouchableOpacity>
+        <Text style={{ fontSize: 18, fontWeight: "400", color: "white" }}>
+          로그인
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
